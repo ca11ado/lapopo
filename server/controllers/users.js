316aec9fc6e85fb028ef97847090991051b34c1b
Igor Grammatchikov <igor.grammatchikov@lamoda.ru>
2020-12-05 10:48:08 +0300

    fix redis

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,25 +1,45 @@
-const Redis = require('koa-redis');
-const Store = new Redis().client;
+const Redis = require("koa-redis");
+const redis = new Redis();
 
-function register (ctx) {
-  const {name, pokers} = ctx.request.body;
-  ctx.session.name = name;
-  ctx.session.pokers = pokers;
-  ctx.status = 201;
+async function register (ctx) {
+  const { name } = ctx.request.body;
+  const keys = await redis.client.keys('*');
+  let values;
+  if (keys.length) {
+    values = await redis.client.mget(keys);
+    const names = values.map(value => {
+      try {
+        return JSON.parse(value).name;
+      } catch (e) {
+        ctx.throw(501)
+      }
+    });
+    if (names.includes(name)) {
+      ctx.throw(409, `name ${name} already exist`);
+    } else {
+      ctx.session.name = name;
+      ctx.status = 201;
+    }
+  } else {
+    ctx.session.name = name;
+    ctx.status = 201;
+  }
 }
 
 async function auth (ctx) {
   if (!ctx.cookies.get("sid")) {
     ctx.throw(401);
   }
-  const infoStr = await Store.get("koa:sess:"+ctx.cookies.get("sid"));
-  console.log(infoStr)
-  if (infoStr && JSON.parse(infoStr).name) {
-    ctx.body = {name : JSON.parse(infoStr).name}
+  const infoStr = await redis.client.get(`koa:sess:${ctx.cookies.get("sid")}`);
+  if (infoStr) {
+    try {
+      ctx.body = JSON.parse(infoStr)
+    } catch (e) {
+      ctx.throw(501)
+    }
   } else {
-    ctx.throw(404);
+    ctx.throw(404)
   }
-  
 }
 
 
--- server/middleware/index.js
+++ server/middleware/index.js
@@ -1,16 +1,23 @@
 const serve = require('koa-static');
 const path = require('path');
+const session = require('koa-generic-session');
+const redisStore = require('koa-redis');
 const router = require('./router');
 const bodyParser = require('koa-bodyparser');
-const session = require('koa-generic-session');
-const Redis = require('koa-redis');
+require('dotenv').config();
 
 module.exports = (app) => {
-  app.keys = ['keys', 'keykeys'];
+  app.keys = [process.env.SECRET_KEY];
   app.use(session({
-    key:'sid',
-    store: Redis(),
-  }));
+    key: 'sid',
+    store: redisStore({
+      serialize: (session) => {
+        return JSON.stringify({
+          name: session.name
+        })
+      },
+    }),
+  }, app));
   app.use(bodyParser());
   app.use(router.middleware());
   if (process.env.NODE_ENV === 'production') {
